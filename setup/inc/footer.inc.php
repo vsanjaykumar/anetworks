16524f88c5477b225d77fd12050b3d98aac48047
root <root@ticket.zoomy.lk>
2020-04-30 14:00:59 +0000

    first commit

---
+++ setup/inc/footer.inc.php
@@ -0,0 +1,12 @@
+
+            <div class="clear"></div>
+        </div> <!-- content -->
+    </div> <!-- wizard -->
+    <div id="footer" class="centered">Copyright &copy; <?php echo date('Y'); ?> <a target="_blank" href="https://osticket.com">osTicket.com</a></div>
+
+    <script type="text/javascript" src="../js/jquery-3.4.0.min.js?609b8d8"></script>
+    <script type="text/javascript" src="../js/jstz.min.js?609b8d8"></script>
+    <script type="text/javascript" src="js/setup.js?609b8d8"></script>
+    <script type="text/javascript" src="js/tips.js?609b8d8"></script>
+</body>
+</html>
